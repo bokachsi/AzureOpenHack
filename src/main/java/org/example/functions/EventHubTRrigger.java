bc8e37e5beb82770a4400b91c4ec43bf4113fac4
Serge <cmd_s@yahoo.com>
2023-03-28 19:07:23 +0200

    revert changes

---
+++ src/main/java/org/example/functions/EventHubTRrigger.java
@@ -0,0 +1,23 @@
+package org.example.functions;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+import java.util.*;
+
+/**
+ * Azure Functions with Event Hub trigger.
+ */
+public class EventHubTRrigger {
+    /**
+     * This function will be invoked when an event is received from Event Hub.
+     */
+    @FunctionName("EventHubTRrigger")
+    public void run(
+        @EventHubTrigger(name = "message", eventHubName = "team2eventhub", connection = "default", consumerGroup = "$Default", cardinality = Cardinality.MANY) List<String> message,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Event Hub trigger function executed.");
+        context.getLogger().info("Length:" + message.size());
+        message.forEach(singleMessage -> context.getLogger().info(singleMessage));
+    }
+}
